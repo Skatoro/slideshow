e33ae5b0df811feded23a6c3cb79753a5b52f81b
Skatoro <73762604+Skatoro@users.noreply.github.com>
2023-02-22 23:42:33 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict"
+
+runSlide();
+
+function runSlide(){
+    let delay = 1000;
+    let currentImage = 0;
+
+    const shoesWrapper = document.querySelector(".shoes-wrapper");
+    const shoes = document.querySelectorAll(".container > *");
+    const imageAmount = shoes.length - 1;
+
+
+
+    setTimeout(function run() {
+
+        shoes[currentImage].style.opacity = "0";
+
+        if (currentImage === imageAmount) {
+            currentImage = 0;
+        } else {
+            currentImage++;
+        }
+        shoes[currentImage].style.opacity = "1";
+
+        setTimeout(run, delay);
+    }, delay);
+}
