7789f85df8d0c39acfb10918f89be1e33ab2f515
Skatoro <73762604+Skatoro@users.noreply.github.com>
2023-03-08 01:43:22 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,56 +6,70 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 function run(){
-    let delay = 3000;
+    let delay = 5000;
     let currentImage = 0;
 
     const shoes = document.querySelectorAll(".container > *");
     const imageAmount = shoes.length - 1;
+    setInvisible()
 
     addPagers();
     const pager = document.querySelectorAll(".pager-container > *");
 
     const nextButton = document.querySelector(".btn-next");
     const prevButton = document.querySelector(".btn-prev");
+    const startButton = document.querySelector(".btn-start");
+    const stopButton = document.querySelector(".btn-stop");
 
     nextButton.addEventListener("click", nextPress);
     prevButton.addEventListener("click", prevPress);
+    startButton.addEventListener("click", runSlide);
+    stopButton.addEventListener("click", stopSlide);
     for (let i = 0; i <= imageAmount; i++) {
         pager[i].onclick = function() {
             pagerPress(this.id);
         };
     }
 
-    shoes[currentImage].style.opacity = "1";
-
     let timer = setTimeout(runSlide, delay);
 
+    function setInvisible() {
+        for (let i = 0; i <= imageAmount; i++) {
+            shoes[i].classList.add("invisible");
+        }
+        shoes[0].classList.remove("invisible");
+    }
     function runSlide() {
-        timer = setTimeout(runSlide, delay)
+        timer = setTimeout(runSlide, delay);
 
         changeSlide("next");
+    }
+    function stopSlide() {
+        clearTimeout(timer);
     }
     function nextPress() {
         clearTimeout(timer);
 
-        changeSlide("next")
-        timer = setTimeout(runSlide, delay)
+        changeSlide("next");
+        timer = setTimeout(runSlide, delay);
     }
     function prevPress() {
         clearTimeout(timer);
 
         changeSlide("prev");
-        timer = setTimeout(runSlide, delay)
+        timer = setTimeout(runSlide, delay);
     }
     function pagerPress(id) {
         clearTimeout(timer);
 
         changeSlide(Number(id));
-        timer = setTimeout(runSlide, delay)
+        timer = setTimeout(runSlide, delay);
     }
     function changeSlide(pointer) {
         pager[currentImage].classList.remove("pager-active");
-        shoes[currentImage].style.opacity = "0";
+        shoes[currentImage].classList.add("invisible");
+        shoes[currentImage].classList.remove("fade-in");
+        shoes[currentImage].classList.add("fade-out");
         if (pointer === "next") {
             currentImage === imageAmount ? currentImage = 0 : currentImage++;
         }
@@ -68,7 +82,10 @@ function run(){
             alert(`wrong pointer`);
         }
         pager[currentImage].classList.add("pager-active");
-        shoes[currentImage].style.opacity = "1";
+        shoes[currentImage].classList.remove("invisible");
+        shoes[currentImage].classList.remove("fade-out");
+        shoes[currentImage].classList.add("fade-in");
+
     }
     function addPagers() {
         for (let i = 0; i <= imageAmount; i++) {
@@ -82,4 +99,4 @@ function run(){
             }
         }
     }
-} 
+}
