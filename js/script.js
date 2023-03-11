f6ebc55a7f6dfab1ec7cb516e257cd09ccd07d3c
Skatoro <73762604+Skatoro@users.noreply.github.com>
2023-03-11 03:07:57 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,103 +7,195 @@ window.addEventListener('DOMContentLoaded', () => {
 
 function run(){
     let delay = 5000;
-    let currentImage = 0;
-    let isStopped = false;
-    let firstActiveImage = 0;
+    let isStopped = [false, false, false];
+    let firstActiveImage = [0, 1, 2];
+    let currentImage = firstActiveImage.slice();
 
-    const shoes = document.querySelectorAll(".container > *");
-    const imageAmount = shoes.length - 1;
-    setInvisible()
+    let galleries = document.querySelectorAll(".gallery-container");
+    let galleriesAmount = galleries.length;
+    setGalleryIndex();
+
+    const imageArrays = fillImageArrays();
+    const imageAmount = fillImageAmount();
+
+    setInvisible();
 
     addPagers();
-    const pager = document.querySelectorAll(".pager-container > *");
+    const pagerArray = fillPagerArray();
+    setPagerIndex();
 
-    const nextButton = document.querySelector(".btn-next");
-    const prevButton = document.querySelector(".btn-prev");
-    const startButton = document.querySelector(".btn-start");
-    const stopButton = document.querySelector(".btn-stop");
+    setButtonListeners();
 
-    nextButton.addEventListener("click", nextPress);
-    prevButton.addEventListener("click", prevPress);
-    startButton.addEventListener("click", runSlide);
-    stopButton.addEventListener("click", stopSlide);
-    for (let i = 0; i <= imageAmount; i++) {
-        pager[i].onclick = function() {
-            pagerPress(this.dataset.number);
-        };
-    }
-
-    let timer = setTimeout(runSlide, delay);
+    let timers = startTimers();
 
     function setInvisible() {
-        for (let i = 0; i <= imageAmount; i++) {
-            shoes[i].classList.add("invisible");
+        for (let i = 0; i < galleriesAmount; i++) {
+            let galleryImageAmount = imageAmount[i];
+            for (let j = 0; j < galleryImageAmount; j++) {
+                let currentImage = imageArrays[i][j];
+                currentImage.classList.add("invisible");
+            }
+            let currentActiveImage = imageArrays[i][firstActiveImage[i]]
+            currentActiveImage.classList.remove("invisible");
         }
-        shoes[firstActiveImage].classList.remove("invisible");
     }
-    function runSlide() {
-        timer = setTimeout(runSlide, delay);
+    function runSlide(galleryIndex) {
+        timers[galleryIndex] = setTimeout(() => runSlide(galleryIndex), delay);
 
-        changeSlide("next");
-        isStopped = false;
+        changeSlide("next", galleryIndex);
     }
-    function stopSlide() {
-        clearTimeout(timer);
-        isStopped = true;
+    function startSlide(galleryIndex) {
+        if (isStopped[galleryIndex] === true) {
+            runSlide(galleryIndex);
+            isStopped[galleryIndex] = false;
+        }
     }
-    function nextPress() {
-        clearTimeout(timer);
+    function stopSlide(galleryIndex) {
+        clearTimeout(timers[galleryIndex]);
+        isStopped[galleryIndex] = true;
+    }
+    function nextPress(galleryIndex) {
+        clearTimeout(timers[galleryIndex]);
 
-        changeSlide("next");
+        changeSlide("next", galleryIndex);
         if (!isStopped) {
-            timer = setTimeout(runSlide, delay);
+            timers[galleryIndex] = setTimeout(() => runSlide(galleryIndex), delay);
         }
     }
-    function prevPress() {
-        clearTimeout(timer);
+    function prevPress(galleryIndex) {
+        clearTimeout(timers[galleryIndex]);
 
-        changeSlide("prev");
+        changeSlide("prev", galleryIndex);
         if (!isStopped) {
-            timer = setTimeout(runSlide, delay);
+            timers[galleryIndex] = setTimeout(() => runSlide(galleryIndex), delay);
         }
     }
-    function pagerPress(datasetNumber) {
-        clearTimeout(timer);
+    function pagerPress(datasetNumber, galleryIndex) {
+        clearTimeout(timers[galleryIndex]);
 
-        changeSlide(Number(datasetNumber));
-        if (!isStopped) {
-            timer = setTimeout(runSlide, delay);
+        changeSlide(Number(datasetNumber), galleryIndex);
+        if (!isStopped[galleryIndex]) {
+            timers[galleryIndex] = setTimeout(() => runSlide(galleryIndex), delay);
         }
     }
-    function changeSlide(pointer) {
-        pager[currentImage].classList.remove("pager-active");
-        shoes[currentImage].classList.add("invisible");
-        shoes[currentImage].classList.remove("fade-in");
-        shoes[currentImage].classList.add("fade-out");
+    function changeSlide(pointer, galleryIndex) {
+        let currentImg = currentImage[galleryIndex];
+
+        pagerArray[galleryIndex][currentImg].classList.remove("pager-active");
+        imageArrays[galleryIndex][currentImg].classList.add("invisible");
+        imageArrays[galleryIndex][currentImg].classList.remove("fade-in");
+        imageArrays[galleryIndex][currentImg].classList.add("fade-out");
         if (pointer === "next") {
-            currentImage === imageAmount ? currentImage = 0 : currentImage++;
+            currentImage[galleryIndex] === imageAmount[galleryIndex] - 1 ?
+                currentImage[galleryIndex] = 0 : currentImage[galleryIndex]++;
         }
         else if (pointer === "prev") {
-            currentImage === 0 ? currentImage = imageAmount : currentImage--;
+            currentImage[galleryIndex] === 0 ?
+                currentImage[galleryIndex] = imageAmount[galleryIndex] - 1 : currentImage[galleryIndex]--;
         } else if (!isNaN(pointer)) {
-            currentImage = pointer;
+            currentImage[galleryIndex] = pointer;
         }
-        pager[currentImage].classList.add("pager-active");
-        shoes[currentImage].classList.remove("invisible");
-        shoes[currentImage].classList.remove("fade-out");
-        shoes[currentImage].classList.add("fade-in");
+        currentImg = currentImage[galleryIndex];
+        pagerArray[galleryIndex][currentImg].classList.add("pager-active");
+        imageArrays[galleryIndex][currentImg].classList.remove("invisible");
+        imageArrays[galleryIndex][currentImg].classList.remove("fade-out");
+        imageArrays[galleryIndex][currentImg].classList.add("fade-in");
 
+        clearExcessFade(galleryIndex);
     }
     function addPagers() {
-        for (let i = 0; i <= imageAmount; i++) {
-            const parent = document.querySelector(".pager-container");
-            const child = document.createElement("div");
-            child.classList.add("pager-unit");
-            child.dataset.number = `${i}`;
-            parent.appendChild(child)
-            if (i === firstActiveImage) {
-                child.classList.add("pager-active");
+        for (let i = 0; i < galleriesAmount; i++) {
+            let galleryImageAmount = imageAmount[i]
+
+            for (let j = 0; j < galleryImageAmount; j++) {
+                const parent = galleries[i].querySelector(".pager-container");
+                const child = document.createElement("div");
+                child.classList.add("pager-unit");
+                child.dataset.number = `${j}`;
+                parent.appendChild(child);
+                if (j === firstActiveImage[i]) {
+                    child.classList.add("pager-active");
+                }
             }
         }
+    }
+    function clearExcessFade(galleryIndex) {            // когда быстро меняешь слайд, появлялось много анимаций fadeOut
+        let previousLeft = currentImage[galleryIndex] - 2;
+        let previousRight = currentImage[galleryIndex] + 2;
+        let currentImageAmount = imageAmount[galleryIndex];
+
+        if (previousLeft < 0) {
+            previousLeft += currentImageAmount;
+        }
+        if (previousRight > currentImageAmount - 1) {
+            previousRight -= currentImageAmount;
+        }
+        imageArrays[galleryIndex][previousLeft].classList.remove("fade-out");
+        imageArrays[galleryIndex][previousRight].classList.remove("fade-out");
+    }
+    function setGalleryIndex(){
+        for (let i = 0; i < galleriesAmount; i++) {
+            galleries[i].dataset.index = `${i}`;
+        }
+    }
+    function fillImageArrays() {
+        let imageArrays = [];
+        for (let i = 0; i < galleriesAmount; i++) {
+            imageArrays.push(galleries[i].querySelectorAll(".photo-container > *"));
+        }
+        return imageArrays;
+    }
+    function fillImageAmount() {
+        let imageAmounts = [];
+        for (let i = 0; i < imageArrays.length; i++) {
+            imageAmounts.push(imageArrays[i].length);
+        }
+        return imageAmounts;
+    }
+    function fillPagerArray() {
+        let pagerArray = [];
+        for (let i = 0; i < galleriesAmount; i++) {
+            pagerArray.push(galleries[i].querySelectorAll(".pager-container > *"));
+        }
+        return pagerArray;
+    }
+    function setPagerIndex() {
+        for (let i = 0; i < galleriesAmount; i++) {
+            let galleryImageAmount = imageAmount[i];
+            let galleryIndex = galleries[i].dataset.index;
+
+            for (let j = 0; j < galleryImageAmount; j++) {
+                let currentPagerUnit = pagerArray[i][j];
+                currentPagerUnit.onclick = function() {
+                    pagerPress(this.dataset.number, galleryIndex);
+                };
+            }
+
+        }
+    }
+    function setButtonListeners() {
+        for (let i = 0; i < galleriesAmount; i++) {
+            let currentGallery = galleries[i];
+            let galleryIndex = galleries[i].dataset.index;
+
+            const nextButton = currentGallery.querySelector(".btn-next");
+            const prevButton = currentGallery.querySelector(".btn-prev");
+            const startButton = currentGallery.querySelector(".btn-start");
+            const stopButton = currentGallery.querySelector(".btn-stop");
+
+            nextButton.addEventListener("click", () => nextPress(galleryIndex));
+            prevButton.addEventListener("click", () => prevPress(galleryIndex));
+            startButton.addEventListener("click", () => startSlide(galleryIndex));
+            stopButton.addEventListener("click", () => stopSlide(galleryIndex));
+        }
+    }
+    function startTimers() {
+        let timers = [];
+
+        for (let i = 0; i < galleriesAmount; i++) {
+            let currentGallery = i;
+            timers.push(setTimeout(() => runSlide(currentGallery), delay));
+        }
+        return timers;
     }
 }
